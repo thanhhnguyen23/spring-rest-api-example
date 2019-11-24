9665a2b1fef868173702541dac2685050a890a0d
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-11-23 17:36:35 -0800

    added validator for title of courses

--- src/main/java/com/thanhhnguyen23/course/Course.java
+++ src/main/java/com/thanhhnguyen23/course/Course.java
@@ -6,12 +6,14 @@ import com.thanhhnguyen23.review.Review;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Course extends BaseEntity {
 
+    @NotNull
     private String title;
     private String url;
     @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
